14f25dd9d47307c0e7de54804c6a579162b8b0f0
Needa Mehmud <needa.mehmud@gmail.com>
2021-07-12 23:02:42 -0400

    added employees pseudo code/tests

---
+++ _tests_/employee.test.js
@@ -0,0 +1,26 @@
+const { describe } = require("yargs");
+const Employee = require("../lib/employee");
+
+describe('rendering employee info', () => {
+
+
+    it("should set name via constructor arguments", () => {
+    });
+
+    it("should set id via constructor argument", () => {
+    });
+
+    it("should set email via constructor argument", () => {
+    });
+
+    it("should get name via getName() class", () => {
+    });
+
+    it("should get id via getId() class", () => {
+    });
+
+    it("should get email via getEmail() class", () => {
+    });
+
+});
+
