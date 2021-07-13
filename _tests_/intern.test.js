37f154878885be561a77e4c7f3c70b38043a6cde
Needa Mehmud <needa.mehmud@gmail.com>
2021-07-12 23:21:39 -0400

    added pseudo code/tests to intern tests

--- _tests_/intern.test.js
+++ _tests_/intern.test.js
@@ -1,1 +1,12 @@
 import intern from "../lib/intern.js";
+
+describe('render intern info', () => {
+    it("should set school via constructor", () => {
+    });
+
+    it("should have getRole() class return intern", () => {
+    });
+
+    it("should get school via getSchool() class", () => {
+    });
+});
