b1cdcd295878c23af337935e0cbce05f0da56f13
Needa Mehmud <needa.mehmud@gmail.com>
2021-07-14 22:27:56 -0400

    got employee.test.js tests passing

--- _tests_/employee.test.js
+++ _tests_/employee.test.js
@@ -1,25 +1,48 @@
-import employee from ("../lib/employee");
+const Employee = require('../lib/Employee');
 
-describe('rendering employee info', () => {
-
+describe('Employee Info', () => {
 
     it("should set name via constructor arguments", () => {
+        const name = "Fatima";
+        const e = new Employee(name);
+        expect(e.name).toBe(name);
     });
 
     it("should set id via constructor argument", () => {
+        const testValue = 10299;
+        const employee = new Employee("Test", testValue);
+        expect(employee.id).toBe(testValue)
     });
 
     it("should set email via constructor argument", () => {
+        const testValue = "test@test.com";
+        const employee = new Employee("Test", 1, testValue);
+        expect(employee.email).toBe(testValue);
     });
 
     it("should get name via getName()", () => {
+        const testValue = "Fatima";
+        const employee = new Employee(testValue);
+        expect(employee.name).toBe(testValue);
     });
 
     it("should get id via getId()", () => {
+        const testValue = 10299;
+        const employee = new Employee("Test", testValue);
+        expect(employee.id).toBe(testValue)
     });
 
     it("should get email via getEmail()", () => {
+        const testValue = "test@test.com";
+        const employee = new Employee("Test", 1, testValue);
+        expect(employee.email).toBe(testValue);
     });
+
+    it("should return \"Employee\" via getRole() ", () => {
+        const testValue = "Employee";
+        const employee = new Employee("Fatima", 1, "test@test.com");
+        expect(employee.getRole()).toBe(testValue);
+      });
 
 });
 
